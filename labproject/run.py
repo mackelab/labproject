fda70693513c1cb71e4c7a63e5cd342023820498
Sebastian Bischoff <sebastian@salzreute.de>
2024-01-30 11:42:06 +0100

    Add test experiment

--- labproject/run.py
+++ labproject/run.py
@@ -1,2 +1,7 @@
+from labproject.experiments import scaling_sliced_wasserstein_samples
+from labproject.plotting import plot_scaling_metric_dimensionality
+
 print("Running experiments...")
+dimensionality, distances = scaling_sliced_wasserstein_samples()
+plot_scaling_metric_dimensionality(dimensionality, distances, "Sliced Wasserstein", "Random Dataset")
 print("Finished running experiments.")
