f9f7cf765c03b6a7e8d4b5666a18d2dc42f2121f
manuelgloeckler <manu.gloeckler@hotmail.de>
2024-02-04 22:31:16 +0100

    Cifar 10 data loaders

--- labproject/data.py
+++ labproject/data.py
@@ -1,8 +1,16 @@
+from typing import Optional
 import torch
 import requests
 from requests.auth import HTTPBasicAuth
 import os
 import functools
+
+from torchvision.datasets import CIFAR10
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, Dataset, Subset
+from torchvision.models import inception_v3
+
+from labproject.embeddings import FIDEmbeddingNet
 
 
 STORAGEBOX_URL = os.getenv("HETZNER_STORAGEBOX_URL")
@@ -84,7 +92,7 @@ def register_dataset(name: str) -> callable:
 
     def decorator(func):
         @functools.wraps(func)
-        def wrapper(n: int, d: int, **kwargs):
+        def wrapper(n: int, d: Optional[int] =None, **kwargs):
 
             assert n > 0, "n must be a positive integer"
             assert d > 0, "d must be a positive integer"
@@ -120,6 +128,41 @@ def get_dataset(name: str) -> torch.Tensor:
     return DATASETS[name]
 
 
+def load_cifar10(n:int, save_path="data", train=True, batch_size=100, shuffle=False, num_workers=1, device="cpu") -> torch.Tensor:
+    """Load a subset of cifar10
+
+    Args:
+        n (int): Number of samples to load
+        save_path (str, optional): Path to save files. Defaults to "data".
+        train (bool, optional): Train or test. Defaults to True.
+        batch_size (int, optional): Batch size. Defaults to 100.
+        shuffle (bool, optional): Shuffle. Defaults to False.
+        num_workers (int, optional): Parallel workers. Defaults to 1.
+        device (str, optional): Device. Defaults to "cpu".
+
+    Returns:
+        torch.Tensor: Cifar10 embeddings
+    """
+    transform = transforms.Compose([
+            transforms.Resize((299, 299)),
+            transforms.ToTensor(),
+            # normalize specific to inception model
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            # Move to GPU if available
+            transforms.Lambda(lambda x: x.to(device if torch.cuda.is_available() else 'cpu'))
+        ])
+    cifar10 = CIFAR10(root=save_path, train=train, download=True, transform=transform)
+    dataloader = torch.utils.data.DataLoader(cifar10, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    dataloader_subset = Subset(dataloader.dataset, range(n))
+    dataloader = DataLoader(dataloader_subset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+    model = inception_v3(pretrained=True)
+    model.fc = torch.nn.Identity()  # replace the classifier with identity to get features
+    model.eval()
+    model = model.to(device if torch.cuda.is_available() else 'cpu')
+    net = FIDEmbeddingNet(model)
+    embeddings = net.get_embeddings(dataloader)
+    return embeddings
+
 # ------------------------------
 
 
@@ -130,3 +173,34 @@ def get_dataset(name: str) -> torch.Tensor:
 @register_dataset("random")
 def random_dataset(n=1000, d=10):
     return torch.randn(n, d)
+
+
+
+
+@register_dataset("cifar10_train")
+def cifar10_train(n=1000, d=10, save_path="data", device="cpu"):
+    
+    assert d is None or d == 2048, "The dimensionality of the embeddings must be 2048"
+    
+    embeddings = load_cifar10(n,save_path=save_path, train=True, device=device)
+    
+    max_n = embeddings.shape[0]
+    
+    assert n <= max_n, f"Requested {n} samples, but only {max_n} are available"
+    
+    return embeddings[:n]
+
+@register_dataset("cifar10_test")
+def cifar10_test(n=1000, d=2048, save_path="data", device="cpu"):
+    
+    assert d == 2048, "The dimensionality of the embeddings must be 2048"
+    
+    embeddings = load_cifar10(n, save_path=save_path, train=False, device=device)
+    
+    
+    
+    max_n = embeddings.shape[0]
+    
+    assert n <= max_n, f"Requested {n} samples, but only {max_n} are available"
+    
+    return embeddings[:n]
